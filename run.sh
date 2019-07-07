066117e36087782c1253a07d995e538de3b9a409
jackstark <jackstark@qq.com>
2019-07-08 02:44:43 +0800

    .

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 #SBATCH -J segmentation
 #SBATCH -p gpu
 #SBATCH -N 1
-#SBATCH --output=out.log
-#SBATCH --error=err.log
+#SBATCH --output=out_%j.log
+#SBATCH --error=err_%j.log
 #SBATCH -t 100:00:00
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 module load anaconda3/5.3.0
 source activate mm
 python train_seg2019.py --train-root ../data/HaN_OAR --save ../mode_saved --batchSz 1 --ngpu 1
